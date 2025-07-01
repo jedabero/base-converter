9b4fa4a5b2cb38ad986eddf28d477a53bf3f19d4
Jeison Berdugo <jedabero20@gmail.com>
2025-07-01 18:24:19 -0500

    Fix build and dev issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: "src",
-  publicDir: "../public",
+  root: ".",
   build: {
-    outDir: "../dist",
+    outDir: "dist",
     emptyOutDir: true
   }
 });
